8b8230afb6569a025bcd494afb53d998936a4ce0
thgmldl0115 <thgmldl0115@gmail.com>
2024-11-11 17:46:31 +0900

    Update bookboard.sql

--- book_board/bookboard.sql
+++ book_board/bookboard.sql
@@ -67,7 +67,53 @@ VALUES ('aaaa', '방금 떠나온 세계', '김초엽', 10500,
 
 --UPDATE tb_book SET b_category=800 WHERE b_isbn='9791160406504';
 
-SELECT b_end_yn, b_title, b_author, 
+SELECT * FROM tb_bookrecord;
+
+-- 읽을책
+SELECT distinct a.b_title, a.b_author, a.b_isbn, 
+     NVL(SUM(b.r_page) OVER(PARTITION BY b.b_isbn), 0) as cur_page,
+     a.b_page,
+     (a.b_page - NVL(SUM(b.r_page) OVER(PARTITION BY b.b_isbn), 0)) as to_page,
+     a.b_create_dt
+FROM tb_book a, tb_bookrecord b
+WHERE a.b_isbn = b.b_isbn(+)
+AND a.user_id = 'aaaa'
+AND a.b_end_yn = 'N'
+ORDER BY a.b_create_dt;
+
+commit;
+
+-- 목표 리스트 조회
+SELECT a.b_end_yn, a.b_title, a.b_author, 
+        CASE WHEN a.b_category = '000' THEN '총류, 컴퓨터과학'
+             WHEN a.b_category = '100' THEN '철학, 심리학, 윤리학'
+             WHEN a.b_category = '200' THEN '종교'
+             WHEN a.b_category = '300' THEN '사회과학'
+             WHEN a.b_category = '400' THEN '어학'
+             WHEN a.b_category = '500' THEN '순수과학'
+             WHEN a.b_category = '600' THEN '기술과학'
+             WHEN a.b_category = '700' THEN '예술'
+             WHEN a.b_category = '800' THEN '문학'
+             WHEN a.b_category = '900' THEN '역사'
+             ELSE '기타'
+        END as b_category, 
+        NVL(a.b_memo,' ') as b_memo,
+        a.b_page, a.b_dicount, 
+        NVL(TO_CHAR(a.b_create_dt, 'YYYY-MM-DD'),' ') as b_create_dt, 
+        NVL(TO_CHAR(a.b_update_dt, 'YYYY-MM-DD'),' ') as b_update_dt
+FROM tb_book a, tb_bookrecord b
+WHERE a.b_isbn = b.b_isbn(+)
+AND a.user_id='aaaa'
+AND a.b_end_yn = 'N';
+
+commit;
+
+-- 읽을 책 조회
+SELECT distinct
+        CASE WHEN b_create_dt IS NULL THEN 'N'
+             ELSE 'Y'
+        END as read_yn
+        , b_title, b_author, 
         CASE WHEN b_category = '000' THEN '총류, 컴퓨터과학'
              WHEN b_category = '100' THEN '철학, 심리학, 윤리학'
              WHEN b_category = '200' THEN '종교'
@@ -80,11 +126,16 @@ SELECT b_end_yn, b_title, b_author,
              WHEN b_category = '900' THEN '역사'
              ELSE '기타'
         END as b_category, 
-        b_page, b_dicount, 
+        NVL(b_memo, ' '),
+        b_page, 
+        NVL(SUM(b.r_page) OVER(PARTITION BY b.b_isbn), 0) as cur_page, 
         NVL(TO_CHAR(b_create_dt, 'YYYY-MM-DD'),' ') as b_create_dt, 
         NVL(TO_CHAR(b_update_dt, 'YYYY-MM-DD'),' ') as b_update_dt
-FROM tb_book
-WHERE user_id='aaaa';
+FROM tb_book a, tb_bookrecord b
+WHERE a.b_isbn = b.b_isbn(+)
+AND a.user_id= 'aaaa'
+AND b_end_yn = 'N'
+ORDER BY b_create_dt;
 
 SELECT b_title, b_author, b_isbn
 FROM tb_book
@@ -124,7 +175,42 @@ SELECT b.b_title, b.b_author
      , b.b_page
 FROM tb_bookrecord a, tb_book b
 WHERE a.b_isbn = b.b_isbn
-AND a.user_id = 'aaaa';
+AND a.user_id = 'aaaa'
+ORDER BY b.b_create_dt, a.r_date;
+
+-- 다 읽은 책 완독여부 update
+UPDATE tb_book a
+SET a.b_end_yn = 'Y'
+WHERE (a.b_isbn, a.b_page) IN (
+                                SELECT b.b_isbn, SUM(b.r_page) OVER(PARTITION BY b.b_isbn)
+                                FROM tb_bookrecord b
+                                WHERE b.b_isbn = a.b_isbn
+                                )
+AND a.user_id='aaaa';
+
+SELECT distinct a.b_title, a.b_page, SUM(b.r_page) OVER(PARTITION BY b.b_isbn)
+FROM tb_book a, tb_bookrecord b
+WHERE b.b_isbn = a.b_isbn;
+
+SELECT * FROM tb_book;
+SELECT * FROM tb_bookrecord;
+
+---기간 업데이트
+UPDATE tb_book a
+SET (a.b_create_dt, a.b_update_dt) = (
+                                        SELECT distinct MIN(r_date) OVER(PARTITION BY b_isbn)
+                                              ,MAX(r_date) OVER(PARTITION BY b_isbn)
+                                        FROM tb_bookrecord b
+                                        WHERE b.b_isbn(+) = a.b_isbn
+                                        AND  b.r_date IS NOT NULL
+                                        )
+WHERE a.user_id='aaaa';
+
+
+---
+commit;
+
+SELECT * FROM tb_book;
 
 UPDATE tb_book SET b_end_yn='Y' WHERE b_isbn='9791160406504';
 UPDATE tb_book SET b_create_dt=TO_DATE('2024-11-09','YYYY-MM-DD') WHERE b_isbn='9791160406504';
@@ -151,6 +237,7 @@ FROM tb_bookrecord a, tb_book b, tb_user c
 WHERE a.b_isbn = b.b_isbn
 AND a.user_id = c.user_id
 AND b.b_end_yn = 'Y'
-AND c.user_id='aaaa';
+AND c.user_id='aaaa'
+ORDER BY b_create_dt;
 
 commit;
